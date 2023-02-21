984d74832f7a4460369ec89e5cdaf967f46f7994
jys9049 <github.com.com/jys9049>
2023-02-21 19:03:07 +0900

    test3

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,8 +7,6 @@ const Header = () => {
     <div className={styled.HeaderContainer}>
       <Link to="/">메인페이지</Link>
       <Link to="/sub">서브페이지</Link>
-      <Link to="/sub2/1">서브2-1페이지</Link>
-      <Link to="/sub2/2">서브2-2페이지</Link>
     </div>
   );
 };
