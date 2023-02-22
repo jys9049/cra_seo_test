eaa06ed6bffdd6b2fe934c570964188aef604558
jys9049 <github.com.com/jys9049>
2023-02-22 09:51:16 +0900

    param test

--- src/pages/SubTwoPage.tsx
+++ src/pages/SubTwoPage.tsx
@@ -10,21 +10,46 @@ interface IMainPage {
 }
 
 const SubTwoPage = ({ data }: IMainPage) => {
+  const [loading, setLoading] = useState<boolean>(true)
   const param = useParams()
   const intParam = Number(param.id)
-  const [metaTag] = useState({
-    title: data[intParam]?.title,
-    description: data[intParam]?.title,
-    image: data[intParam]?.thumbnailUrl,
+
+  console.log(data)
+
+  const [metaTag, setMetaTag] = useState({
+    title: '',
+    description: '',
+    image: '',
   })
+
+  useEffect(() => {
+    setLoading(true)
+    if (intParam) {
+      if (data) {
+        const test = {
+          title: data[intParam].title,
+          description: data[intParam].title,
+          image: data[intParam].thumbnailUrl,
+        }
+        setMetaTag(test)
+        setLoading(false)
+      }
+    }
+  }, [data, intParam, loading])
 
   return (
     <>
-      <MetaTags {...metaTag} />
-      <div style={{ display: "flex", flexDirection: "column", marginTop: "40px" }}>
-        {/* {/* {`서브${params.id} 페이지 입니다.`} */}
-        <img src={data[intParam].thumbnailUrl} alt="강아지" width="150px" height="150px" />
-      </div>
+      {loading === false ?
+        <>
+          <MetaTags {...metaTag} />
+          <div style={{ display: "flex", flexDirection: "column", marginTop: "40px" }}>
+            {`서브${param.id} 페이지 입니다.`}
+            <img src={metaTag.image} alt="강아지" width="150px" height="150px" />
+          </div>
+        </>
+        :
+        <div>Loading...</div>
+      }
     </>
   );
 };
